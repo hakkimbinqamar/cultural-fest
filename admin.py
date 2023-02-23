f4abee1fb4190b4fbacd6d5ea0fe0cdaefde1243
hakkimbinqamar <124647324+hakkimbinqamar@users.noreply.github.com>
2023-02-23 12:48:45 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Student)
+admin.site.register(Judge)
+admin.site.register(Venue)
+admin.site.register(Event)
+admin.site.register(Accomodation)
+admin.site.register(Result)
